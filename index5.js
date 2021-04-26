95b59d915c2cc99db79f392acbab52fa8ed1a315
ramonju <ramonju1985@gmail.com>
2021-04-26 09:37:12 -0500

    exercise 5

---
+++ index5.js
@@ -0,0 +1,11 @@
+var firstName="Jay"
+var lastName="Puno"
+var age=36
+var address="621 Lansdowne Ave"
+var dateOfBirth="12/12/1985"
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(dateOfBirth);
